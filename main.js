575f8a162bfd863ee4b0e4de68696662b1d9938d
joacomesa22 <joacomesa@gmail.com>
2022-10-11 15:28:26 -0300

    movies

--- main.js
+++ main.js
@@ -24,3 +24,50 @@ function animacionBarras() {
     line2.classList.toggle("btn__line2-active")
     line3.classList.toggle("btn__line3-active")
 }
+
+// Slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    // acá define qué elemento entra en el slider
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = x.length
+    }
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    x[slideIndex-1].style.display = "block";
+}
+
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+
+    if (slideIndex > x.length) {
+        slideIndex = 1
+    }
+
+    x[slideIndex-1].style.display = "block";
+    
+    setTimeout(carousel, 2000); // Cambia la imagen cada 2 segundos
+}
